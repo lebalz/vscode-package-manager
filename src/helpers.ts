65733b4dcf2caccc5f2061e8759585e6118d93e6
Balthasar Hofer <lebalz@outlook.com>
2020-07-09 10:57:51 +0200

    add Progress interface

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,10 @@
 import * as vscode from "vscode";
 
+export type Progress = vscode.Progress<{
+  message?: string | undefined;
+  increment?: number | undefined;
+}>;
+
 export function promptRootPassword(): Thenable<string | undefined> {
   return vscode.window.showInputBox({
     password: true,
